acb840a04243aec2dd1015bad48255f9a07e4b38
Crystal C <crystal.contr@gmail.com>
2019-10-12 22:48:17 -0500

    Removed arguments in main.  Returns current directory where WebServer is started.

--- MyWebServer.java
+++ MyWebServer.java
@@ -1,6 +1,6 @@
 /***  --------------------------------------------------------
  * 1. Name: CRYSTAL A. CONTRERAS 
- *    Date: 10/6/2019
+ *    Date: 10/12/2019
  * 2. Java version used:
  * openjdk version "1.8.0_222"
  * OpenJDK Runtime Environment (AdoptOpenJDK)(build 1.8.0_222-b10)
@@ -10,65 +10,7 @@
  * > javac *.java
  * 
  * 4. Precise examples / instructions to run this program:
- * TO RUN TELNET:
- * I installed the telnet application using homebrew:
- * > brew install telnet
- * 
- * After it finished installing, I tested it by entering:
- * > telnet condor.depaul.edu 80
- * 
- * This is the response I received:
- * 
-        * Crystals-MacBook:MyWebServer crystalcontreras$ telnet condor.depaul.edu 80
-        * Trying 216.220.180.150...
-        * Connected to condor.depaul.edu.
-        * Escape character is '^]'.
-        * ^] 
-        * HTTP/1.1 400 Bad Request
-        * Date: Sun, 06 Oct 2019 19:20:34 GMT
-        * Server: Apache
-        * Content-Length: 226
-        * Connection: close
-        * Content-Type: text/html; charset=iso-8859-1
-        * 
-        * <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-        * <html><head>
-        * <title>400 Bad Request</title>
-        * </head><body>
-        * <h1>Bad Request</h1>
-        * <p>Your browser sent a request that this server could not understand.<br />
-        * </p>
-        * </body></html>
-        * Connection closed by foreign host.
- * 
- * Apparently, I was not the only student to get this response.
- *
- * Next, I compiled and ran MyWebServer:
- * > javac *.java
  * > java MyWebServer
- * 
- * On the Firefox browser's url, I entered:     http://localhost:2540/TEST
- * 
- * This is the response I received on MyWebServer:
- * 
-        * Crystals-MacBook:MyWebServer crystalcontreras$ java MyWebServer
-        * Clark Elliott's MyWebServer Listener 1.8 starting up, listening at port 2540.
-        * 
-        * GET /TEST HTTP/1.1
-        * Host: localhost:2540
-        * User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:69.0) Gecko/20100101 Firefox/69.0
-        * Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*\/*;q=0.8         <-- inserted backslash before the forwardslash to prevent comment section from closing out
-        * Accept-Language: en-US,en;q=0.5
-        * Accept-Encoding: gzip, deflate
-        * Connection: keep-alive
-        * Upgrade-Insecure-Requests: 1
- * 
- * 
- * After recording the request and updating my code to match a similar response, 
- * I compile my code again and from here on out enter my local pathname for which MyWebServer
- * should read from:
- * > javac *.java
- * > java MyWebServer 2540 /Users/crystalcontreras/Desktop/DePaul/2019Autumn/Distributed_Systems_CSC435/MyWebServer
  * 
  * 5. List of files needed for running the program, all of which are in the same directory.
  * a. checklist-mywebserver.html
@@ -84,7 +26,7 @@ import java.util.*;
 
 class ListenWorker extends Thread {
     Socket sock1;
-    String localPath;
+    String localPath;   // the directory path for which MyWebServer is running
 
     ListenWorker (Socket s, String path) {	
         sock1 = s;
@@ -106,7 +48,6 @@ class ListenWorker extends Thread {
             String sockdata; 
             String request;// CL Header Request
 
-            // log(sock1, request);
            
             while (true) {
                 sockdata = in.readLine (); // CL Header Request
@@ -351,17 +292,11 @@ class ListenWorker extends Thread {
 public class MyWebServer {
     public static boolean controlSwitch = true;
     public static void main(String args[]) throws IOException {
-        // read arguments.  If 2 arguments are not provided, return with usage syntax and close out of program.
-        if (args.length!=2) {
-            System.out.println("Usage: java FileServer <port> <wwwhome>");
-            System.exit(-1);
-        }
+        Socket sock1;       // creates client socket
+        int q_len = 6;	    // # of requests for operating system to queue up
+        int port = 2540;    // Port number for which localhost should bind to
+        String wwwhome = System.getProperty("user.dir");    // Grabs users current directory
 
-        Socket sock1; // creates client socket
-        int q_len = 6;	// # of requests for operating system to queue up
-        int port = Integer.parseInt(args[0]);   // User must enter port number 2540 as the first argument
-        String wwwhome = args[1];       // User must enter the root directory for the files and folders they want dynamically returned.
-        
         ServerSocket serverSock = new ServerSocket(port, q_len);  // creates new server socket using port number and number of requests
 
         System.out.println("Clark Elliott's MyWebServer Listener 1.8 starting up, listening at port " + port + ".\n"); // Prints out to terminal running InetServer
